cada6b8894a67a37470f4c0e1024865036761e11
Francisco Alvarado <ronald.alvarado@epn.edu.ec>
2020-06-16 17:59:26 -0500

    Aregglos introduccion

---
+++ 01-javascript/02-arreglos.js
@@ -0,0 +1,9 @@
+const arreglo = [1,2,3,4,5,6,7,8,9,10];
+//arreglo = 1;
+let cualquierCosa = "ASD";
+cualquierCosa = 1;
+cualquierCosa = true;
+//Podemos crear un arreglo de varios tipos
+const arregloTodo = [
+    true, 1, 1.2, "",undefined, null 
+];
