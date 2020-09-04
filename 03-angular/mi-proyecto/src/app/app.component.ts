2d894ab177c7b947b9ebebbea4b6386cecee14b7
Francisco Alvarado <ronald.alvarado@epn.edu.ec>
2020-09-04 18:07:16 -0500

    Directivas y comunicacion entre el back y e front

--- 03-angular/mi-proyecto/src/app/app.component.ts
+++ 03-angular/mi-proyecto/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {UsuarioService} from "./componentes/servicios/http/usuario.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mi-proyecto';
+
+  arregloPeliculas = [
+    {
+      id:1,
+      url:'',
+      descripcion: 'Mas barato por docena',
+      nombrePelicula: 'Huy se frejo'
+    },
+    {
+      id:2,
+      url:'',
+      descripcion: 'Menos barato por docena',
+      nombrePelicula: 'Yala'
+    },
+    {
+      id:3,
+      url:'',
+      descripcion: 'Lo mismo pero mas barato',
+      nombrePelicula: 'Ni idea'
+    }
+  ]
+
+  arregloNumeros = [1,2,3]
+
+  // Inyectar Dependencias
+  constructor(
+    private readonly _usuarioService: UsuarioService
+  ) {
+  }
+  mensajeConsola(objeto: boolean) {
+    console.log('Llego el evento', objeto);
+    const observableTraerTodos = this._usuarioService.traerTodos();
+    observableTraerTodos
+      .subscribe(
+        (data)=>{ // THEN TRY
+          console.log(data);
+        },
+        (error)=>{ // CATCH
+          console.log(error);
+        }
+      );
+  }
 }
