dc8d430fa7cc88ab8d5044e222514909f39ae7c8
Francisco Alvarado <ronald.alvarado@epn.edu.ec>
2020-09-11 17:59:42 -0500

    rutas y servicios delete

--- 03-angular/mi-proyecto/src/app/app-routing.module.ts
+++ 03-angular/mi-proyecto/src/app/app-routing.module.ts
@@ -2,15 +2,44 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {RutaInicioComponent} from "./rutas/ruta-inicio/ruta-inicio.component";
 import {RutaLoginComponent} from "./rutas/ruta-login/ruta-login.component";
+import {RutaUsuarioComponent} from "./rutas/ruta-usuario/ruta-usuario.component";
+import {RutaListaUsuarioComponent} from "./rutas/ruta-lista-usuario/ruta-lista-usuario.component";
+import {RutaEditarUsuarioComponent} from "./rutas/ruta-editar-usuario/ruta-editar-usuario.component";
+import {RutaCrearUsuarioComponent} from "./rutas/ruta-crear-usuario/ruta-crear-usuario.component";
 
-const routes: Routes = [{
-  component: RutaInicioComponent,  //COMPONENT
-  path: 'inicio' //URL
-},
+const routes: Routes = [
+  {
+    component: RutaInicioComponent,  //COMPONENT
+    path: 'inicio' //URL
+  },
   {
     component: RutaLoginComponent,
     path: 'login'
-  },{
+  },
+  {
+    component: RutaUsuarioComponent,
+    path: 'usuario',
+    children: [
+      {
+          path: 'lista',
+        component: RutaListaUsuarioComponent
+      },
+      {
+        path: 'crear',
+        component: RutaCrearUsuarioComponent
+      },
+      {
+        path: 'editar/:id',
+        component: RutaEditarUsuarioComponent
+      },
+      {
+        path: '',
+        redirectTo: 'lista',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
     path: '',
     redirectTo: '/inicio',
     pathMatch: 'full'
--- 03-angular/mi-proyecto/src/app/componentes/servicios/http/usuario.service.ts
+++ 03-angular/mi-proyecto/src/app/componentes/servicios/http/usuario.service.ts
@@ -19,8 +19,15 @@ export class UsuarioService {
     return this._httpClient.post(this.url + '/Usuario', usuario);
   }
 
+  obtenerUnoPorId(idUsuario: number){
+    return this._httpClient.get(this.url + '/Usuario/' + idUsuario);
+  }
 
-
+  eliminar(idUsuario: number){
+    return this._httpClient.delete(
+      this.url+'/Usuario/'+ idUsuario
+    )
+  }
 
 
 }
